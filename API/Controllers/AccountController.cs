990f382b0dea6eb859ecf018d81e2a4847bc6dbf
iampurna <linkwithpurna@gmail.com>
2024-04-20 13:10:00 +0545

    created AccontController

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //api/account
+    public class AccountController: ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<AppUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration)
+        {
+             _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            
+        }
+    }
+}
--- API/Migrations/20240420070800_initial.Designer.cs
+++ API/Migrations/20240420070800_initial.Designer.cs
@@ -12,7 +12,7 @@ namespace API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20240420070800_initial")]
-    partial class initial
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
